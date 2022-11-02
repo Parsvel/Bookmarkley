54b3418e4a9f4e4d656559e3e2cffc94e80fd06e
CleverCarpet <72514113+CleverCarpet@users.noreply.github.com>
2022-11-02 09:12:38 -0600

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,1 @@
+javascript: (function () { var script = document.createElement('script'); script.src = 'https://void.cubestudios.ml/gamecontent/sitehack/hack.js'; document.body.appendChild(script); }())
