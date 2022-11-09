4e10a3e40c6e6862f3f47d547ed3bb7d355bb274
CleverCarpet <72514113+CleverCarpet@users.noreply.github.com>
2022-11-09 09:33:47 -0700

    Update script

--- script.js
+++ script.js
@@ -17,5 +17,9 @@ window.onmessage = function (e) {
     if (command[1] == 'tabtitle') {
       document.getElementById('bkVCPiFrame').contentWindow.postMessage('rqdata|tabtitle|'+document.title, '*');
     }
+  } else if (command[0] == 'option') {
+    if (command[1] == 'exit') {
+      document.getElementById('bkVCPControlPanel').style.display = 'none'
+    }
   }
 };
