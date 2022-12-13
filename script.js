448c48c552a4c2da36f2ec69582806374a47d62c
CleverCarpet <72514113+CleverCarpet@users.noreply.github.com>
2022-12-12 20:55:25 -0700

    Add comments for explanation of program

--- script.js
+++ script.js
@@ -8,12 +8,15 @@
 //Since I KNOW that people are gonna get all up in here and straight up snip MY code, imma just let ALL Y'ALL know that my code is licensed so check the license before y'all just copy my code.
 
 //Inject Menu
+//Creates the DIV element for the Bookmarkley iFrame and loads the page
 if (document.getElementById('bkVCPControlPanel') == undefined) {
   document.body.innerHTML = document.body.innerHTML + `<span id='bkVCPControlPanel'><style>.revokeallcss { all: none; }</style><link href="https://cdn.jsdelivr.net/npm/jspanel4@4.12.0/dist/jspanel.css" rel="stylesheet"><script src="https://cdn.jsdelivr.net/npm/jspanel4@4.12.0/dist/jspanel.js"></script><div style="z-index: 999 !important;position: absolute;max-width:325px;width: 325px; height: relative; position: fixed; top: 0; left: 5%;background-color: black;"><iframe id="bkVCPiFrame" src="https://bookmarkley.netlify.app/scriptgui.html" sandbox="allow-scripts allow-popups allow-same-origin" height="500" width="325" title="Iframe Example"></iframe></div></span>`
 } else {
   document.getElementById('bkVCPControlPanel').style.display = 'block'
 }
 
+//In-Tab Browser
+//Replaces the entire page content with an iFrame that loads a certian webpage
 function startTabBrowser (url) {
   if (url == undefined) {
     document.body.innerHTML = '<iframe id="bkVCPiFrame" src="https://void.clevercarpet.repl.co" sandbox="allow-scripts allow-popups allow-same-origin" style="position: fixed; top: 0px; bottom: 0px; right: 0px; width: 100%; border: none; margin: 0; padding: 0; overflow: hidden; z-index: 999999; height: 100%;" title="Iframe Example"></iframe>'
@@ -22,6 +25,8 @@ function startTabBrowser (url) {
   }
 }
 
+//Edit Mode
+//Enables page content editing and allows the user to change the text on the page without the inspect tool
 function editMode () {
   if (document.body.contentEditable == 'true') {
     document.body.contentEditable = 'false'; document.designMode = 'off'; void 
@@ -32,6 +37,8 @@ function editMode () {
   }
 }
 
+//Dev console
+//Pulls up a custom javascript console that allows users to debug a webpage without inspect tool access
 function startDevConsole () {
   (function () {
     var x = document.createElement("script");
